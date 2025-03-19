def15ebbeb41f86fa30c89c03b7eebe1fb5d614e
MariaJulia <mariajuliadev@gmail.com>
2025-03-19 11:33:58 -0300

    Adicionando footer

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,19 @@
+import styles from '../../styles/footer.module.css';
+import githubLog from '../../assets/github-logo.png';
+import linkedinLogo from '../../assets/linkedin.png';
+
+export function Footer() {
+    return (
+        <footer className={styles.footerContainer}>
+            <div className={styles.infosFooter}>
+                <span>Desenvolvido por Maria Júlia © 2025</span>
+
+                <div className={styles.footerLinks}>
+                    <a href="https://github.com/MariaJuliaAS" target='_blank'><img src={githubLog} alt="Logo github" /></a>
+                    <a href="https://www.linkedin.com/in/maria-julia-araujo/" target='_blank'><img src={linkedinLogo} alt="Logo linkedin" /></a>
+                </div>
+
+            </div>
+        </footer>
+    )
+}
